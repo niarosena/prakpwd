f85da7f1389d6b04777f438d8cc514c5775d7878
niarosena <92257108+niarosena@users.noreply.github.com>
2021-12-03 19:13:32 +0700

    Add files via upload

---
+++ pwd4/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+//Fungsi dibawah ini merupakan bagian internal PHP untuk membuat koneksi baru ke server MySQL. Dengan database akademik2
+$host="localhost";  // Menyiapkan variabel 'host' untuk mendefinisikan nama server
+$username="root"; // Menyiapkan variabel 'user' untuk mendefinisikan nama user database MySQL
+$password=""; // Menyiapkan variabel 'password' untuk mendefinisikan password database MySQL
+$databasename="pwd5"; // Menyiapkan variabel 'database' untuk mendefinisikan nama database MySQL
+
+$con = @mysqli_connect($host,$username,$password,$databasename); // membuat koneksi pada database 
+
+if(!$con){
+	// Function mysqli_connect() akan mencoba untuk terhubung ke database sesuai value dari variabel. Jika koneksi tidak berhasil, pesan seperti di bawah ini akan muncul.
+	echo "Error:".mysqli_connect_error();
+	exit();
+}
+?>
